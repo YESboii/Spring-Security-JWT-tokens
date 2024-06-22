68744cd7f40c7c711f81f15538e2e9dc40171270
ayush <ayush.gupta9635@gmail.com>
2024-06-22 12:27:38 +0530

    initialising repo

---
+++ src/main/java/com/example/JWTSecurity/JwtSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.example.JWTSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.authentication.AuthenticationManager;
+
+@SpringBootApplication
+public class JwtSecurityApplication {
+
+	private AuthenticationManager
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityApplication.class, args);
+	}
+
+}
